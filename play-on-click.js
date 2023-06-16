f08de2a0ac3e01a130257a5147de0be647e7834b
rashikapr <74289013+rashikapr@users.noreply.github.com>
2023-06-16 12:46:30 +0530

    Update play-on-click.js

--- play-on-click.js
+++ play-on-click.js
@@ -1,4 +1,12 @@
 AFRAME.registerComponent("play-on-click", {
- //Add code here
-  
+  schema: {
+    isPlaying: { type: "boolean", default: false }
+  },
+
+  init: function() {
+    this.videoEl = this.el.getAttribute("material").src;
+    this.onClick = this.onClick.bind(this);    
+  },
+
+  //Add your code here
 });
